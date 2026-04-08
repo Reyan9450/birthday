0915da2ee1fefca59f4fcb611843afd1ab484f01
Reyan9450 <reyans9450@gmail.com>
2026-04-08 09:10:04 +0530

    new chnagges

--- romantic-birthday-app/vite.config.ts
+++ romantic-birthday-app/vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["birthday-arft.onrender.com"],
+    host: '0.0.0.0',
+    allowedHosts: [
+      'birthday-arft.onrender.com'
+    ]
   },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'birthday-arft.onrender.com'
+    ]
+  },
+  test: {
+    environment: 'jsdom'
+  }
 })
